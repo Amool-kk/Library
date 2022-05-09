55939aaf60505a89dfa54ac04b26f5332c7cf7f0
Amool-kk <amoolkumar10@gmail.com>
2022-05-09 12:53:29 +0530

    update and fetch books from databse

--- controller/controller.js
+++ controller/controller.js
@@ -2,16 +2,30 @@ const userDb = require('../database/model');
 const bcrypt = require('bcrypt');
 const books = require('../database/booksmodel')
 
-const getbooksData = async (email)=>{
-    const result = await books.find({id:email});
+const getbooksData = async (email) => {
+    const result = await books.find({ id: email });
     return result;
 }
 
-exports.create = async (req,res)=>{
-    const {name,image,author,pages,price} = req.body;
-    
-    if(!name || !image || !author || !pages || !price){
-        res.status(400).send({message : "All detials are required!"});
+exports.read = async (req, res) => {
+    const booksdata = await getbooksData(req.user.email);
+    res.status(200).send({books : booksdata})
+}
+
+exports.readOne = async (req,res)=>{
+    const {id} = req.body
+    const booksdata = await books.findOne({_id : id})
+
+    console.log(booksdata)
+
+    res.status(200).send({book : booksdata})
+}
+
+exports.create = async (req, res) => {
+    const { name, image, author, pages, price } = req.body;
+
+    if (!name || !image || !author || !pages || !price) {
+        res.status(400).send({ message: "All detials are required!" });
     }
 
     try {
@@ -25,78 +39,107 @@ exports.create = async (req,res)=>{
         })
         const result = await newBook.save();
 
-        res.status(200).send({message : "Your book is created!"})
+        res.status(200).send({ message: "Your book is created!" })
     } catch (error) {
-        res.status(400).send({message : error.message || "Some error occured please try again!"});
-    }    
+        res.status(400).send({ message: error.message || "Some error occured please try again!" });
+    }
 }
 
-exports.delete = async (req,res)=>{
-    const {name} = req.body;
+exports.delete = async (req, res) => {
+    const { id } = req.body;
 
-    if(!name){
-        res.status(400).send({message : "Name of book can not empty!"})
+    if (!id) {
+        res.status(400).send({ message: "ID of book can not empty!" })
     }
 
-    const whichBook = await books.find({name: name});
+    const whichBook = await books.find({ _id : id });
 
-    try{
+    try {
         const deleteBook = await books.deleteOne(
-            {"id" : req.user.email, "name" : name}
+            { "id": req.user.email, "_id": id }
         )
 
-        res.status(200).send({message : "Your book is deleted"})
-    }catch(err){
-        res.status(400).send({message : err.message || "Some error occured please try again"})
+        res.status(200).send({ message: "Your book is deleted" })
+    } catch (err) {
+        res.status(400).send({ message: err.message || "Some error occured please try again" })
+    }
+}
+
+exports.update = async (req, res) => {
+    console.log(req.body)
+    const {id, name, image, author, pages, price } = req.body;
+
+    if (!id) {
+        res.status(400).send({ message: "ID of book can not empty!" })
+    }
+
+    
+    try {
+        const deleteBook = await books.updateOne(
+            { "_id": id},
+            {
+                $set : {
+                    name,
+                    image,
+                    author,
+                    pages,
+                    price
+                }
+            }
+        )
+
+        res.status(200).send({ message: "Your book is Updated" })
+    } catch (err) {
+        res.status(400).send({ message: err.message || "Some error occured please try again" })
     }
 }
 
 
-exports.register = (req,res)=>{
-    if(!req.body){
-        res.status(400).send({message : "Request can not be empty!"})
+exports.register = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({ message: "Request can not be empty!" })
     }
     const user = new userDb({
-        email : req.body.email,
-        password : req.body.password
+        email: req.body.email,
+        password: req.body.password
     })
 
     // const token = await user.genrateToken();
 
-    user.save(user).then(data =>{
+    user.save(user).then(data => {
         res.send(data)
-    }).catch(err=>{
-        res.status(500).send({message : err.message || "Some error occured please try again!"})
+    }).catch(err => {
+        res.status(500).send({ message: err.message || "Some error occured please try again!" })
     })
 }
 
 
-exports.login = async (req,res)=>{
-    const {email,password}=req.body;
-    if(!email || !password){
-        res.status(400).send({message : "Request can not be empty!"})
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400).send({ message: "Request can not be empty!" })
     }
 
-    const userExist = await userDb.findOne({email:email})
+    const userExist = await userDb.findOne({ email: email })
 
-    if(userExist){
+    if (userExist) {
         const isMatch = await bcrypt.compare(password, userExist.password)
-        if(isMatch){
+        if (isMatch) {
             const token = await userExist.genrateToken();
-            res.cookie('jwt',token,{
-                httpOnly : true
+            res.cookie('jwt', token, {
+                httpOnly: true
             })
             const results = await userExist.save()
-            res.status(200).send({message : "Login Successfuly"});
-        }else{
-            res.status(400).send({message : "User details is not correct!"});
+            res.status(200).send({ message: "Login Successfuly" });
+        } else {
+            res.status(400).send({ message: "User details is not correct!" });
         }
-    }else{
-        res.status(400).send({message : "User not found!"});
+    } else {
+        res.status(400).send({ message: "User not found!" });
     }
 }
 
-exports.logout = (req,res)=>{
+exports.logout = (req, res) => {
     res.clearCookie('jwt')
     res.status(200).send('user logout')
 }
--- router.js
+++ router.js
@@ -4,7 +4,8 @@ const jwt = require('jsonwebtoken');
 const auth = require('./middleware/auth');
 const User = require('./database/model')
 const controller = require('./controller/controller')
-const path = require('path')
+const path = require('path');
+const books = require('./database/booksmodel');
 
 const router = express.Router()
 
@@ -14,15 +15,27 @@ router.get('/',auth,(req,res)=>{
     res.status(200).json({message:"ok"})
 })
 
+// for all books of login user
+router.get('/api/books',auth,controller.read);
 
+// for registeration (require email and password)
 router.post('/api/register',controller.register);
 
+// for login (require email and password)
 router.post('/api/login',controller.login);
 
 router.get('/api/logout',controller.logout);
 
+// for create new book (require the details of book)
 router.post('/api/create',auth,controller.create);
 
+// for delete the book (require _id of that book)
 router.post('/api/delete',auth,controller.delete);
+
+// for update the book (require _id of that book other those details of book for update)
+router.post('/api/update',auth,controller.update);
+
+// for one book of login user (require _id of that book)
+router.post('/api/book',auth,controller.readOne);
 
 module.exports = router;
